c857e49bd715084a2aad57f396facf285b0da71b
mefistotelis <mefistotelis@gmail.com>
2025-11-03 10:19:53 +0100

    syndwarsfx: Remade show_world_city_info_box()

--- src/feworld.c
+++ src/feworld.c
@@ -24,10 +24,12 @@
 #include "bfstrut.h"
 #include "poly.h"
 #include "ssampply.h"
+
 #include "femain.h"
 #include "guiboxes.h"
 #include "guitext.h"
 #include "display.h"
+#include "game_data.h"
 #include "game_speed.h"
 #include "game_sprts.h"
 #include "game.h"
@@ -48,6 +50,7 @@ extern struct TbSprite *fe_icons_sprites;
 
 extern short word_155110[6];
 extern ubyte byte_15511C;// = 1;
+extern short word_155744[6];
 extern long landmap_8BC;
 extern long landmap_8C0;
 extern long landmap_8C4;
@@ -93,13 +96,84 @@ ubyte do_unkn2_ACCEPT(ubyte click)
 
 ubyte show_world_city_info_box(struct ScreenTextBox *p_box)
 {
+#if 0
     ubyte ret;
     asm volatile ("call ASM_show_world_city_info_box\n"
         : "=r" (ret) : "a" (p_box));
     return ret;
+#endif
+    ushort i;
+    int n_lines;
+    int tx_height, ln_height, scr_y;
+
+    if (unkn_city_no == -1)
+        return 0;
+
+    if ((p_box->Flags & 0x0080) != 0)
+    {
+        for (i = 0; i < 6 ; i++) {
+            word_155744[i] = -5;
+        }
+        p_box->Flags &= ~0x0080;
+        world_info_ACCEPT_button.Flags |= 0x0001;
+        world_info_CANCEL_button.Flags |= 0x0001;
+    }
+
+    if (p_box->Timer == 255)
+    {
+        const char *text;
+
+        for (i = 0; i < 6 ; i++) {
+            text = (const char *)&memload[cities[unkn_city_no].TextIndex[i]];
+            word_155744[i] = strlen(text);
+        }
+        world_info_CANCEL_button.Flags |= 0x0002;
+        world_info_ACCEPT_button.Flags |= 0x0002;
+        p_box->Timer = -2;
+    }
+
+    lbDisplay.DrawFlags |= 0x0100;
+    lbFontPtr = small_med_font;
+    tx_height = font_height('A');
+    my_set_text_window(p_box->X + 3, p_box->Y + 4, p_box->Width - 6, p_box->Height - 8);
+    scr_y = 4;
+    ln_height = tx_height + 4;
+    for (i = 0; i < 6; i++)
+    {
+        const char *text;
+
+        if (i == 4) {
+            if (language_3str[0] == 'f' || (language_3str[0] == 's' && language_3str[1] != 'w'))
+                continue;
+        }
+        if (i == 0) {
+            text = gui_strings[404];
+        } else {
+            text = gui_strings[489 + i];
+        }
+        draw_text_purple_list2(0, scr_y, text, 0);
+        n_lines = my_count_lines(text);
+        scr_y += ln_height * n_lines;
+        text = (const char *)&memload[cities[unkn_city_no].TextIndex[i]];
+        flashy_draw_text(0, scr_y, text, p_box->TextSpeed, 0, &word_155744[i], 0);
+        n_lines = my_count_lines(text);
+        scr_y += tx_height + ln_height * n_lines;
+    }
+    lbDisplay.DrawFlags = 0;
+
+    if (login_control__State != 5 && screentype == SCRT_WORLDMAP)
+    {
+        //world_info_ACCEPT_button.DrawFn(&world_info_ACCEPT_button); -- incompatible calling convention
+        asm volatile ("call *%1\n"
+            :  : "a" (&world_info_ACCEPT_button), "g" (world_info_ACCEPT_button.DrawFn));
+        //world_info_CANCEL_button.DrawFn(&world_info_CANCEL_button); -- incompatible calling convention
+        asm volatile ("call *%1\n"
+            :  : "a" (&world_info_CANCEL_button), "g" (world_info_CANCEL_button.DrawFn));
+    }
+    return 0;
 }
 
-TbBool draw_world_landmass_projector_startup(struct ScreenBox *box)
+TbBool draw_world_landmass_projector_startup(struct ScreenBox *p_box)
 {
     short i;
 
@@ -108,8 +182,8 @@ TbBool draw_world_landmass_projector_startup(struct ScreenBox *box)
         short tx, ty;
         short x2, y2;
 
-        tx = box->Timer2 * (word_1C488A[i] + box->X - proj_origin.X);
-        ty = box->Timer2 * (word_1C4896[i] + box->Y - proj_origin.Y);
+        tx = p_box->Timer2 * (word_1C488A[i] + p_box->X - proj_origin.X);
+        ty = p_box->Timer2 * (word_1C4896[i] + p_box->Y - proj_origin.Y);
         x2 = proj_origin.X + (tx / 24);
         y2 = proj_origin.Y + (ty / 24);
 
@@ -118,8 +192,8 @@ TbBool draw_world_landmass_projector_startup(struct ScreenBox *box)
         lbDisplay.DrawFlags = 0;
         draw_sprite_purple_list(x2 - 1, y2 - 1, &fe_mouseptr_sprites[12]);
 
-        tx = box->Timer2 * (word_1C48A2[i] + box->X - proj_origin.X);
-        ty = box->Timer2 * (word_1C48AE[i] + box->Y - proj_origin.Y);
+        tx = p_box->Timer2 * (word_1C48A2[i] + p_box->X - proj_origin.X);
+        ty = p_box->Timer2 * (word_1C48AE[i] + p_box->Y - proj_origin.Y);
         x2 = proj_origin.X + (tx / 24);
         y2 = proj_origin.Y + (ty / 24);
 
@@ -128,11 +202,11 @@ TbBool draw_world_landmass_projector_startup(struct ScreenBox *box)
         lbDisplay.DrawFlags = 0;
         draw_sprite_purple_list(x2 - 1, y2 - 1, &fe_mouseptr_sprites[12]);
     }
-    box->Timer2 += box->DrawSpeed;
-    return (box->Timer2 >= 24);
+    p_box->Timer2 += p_box->DrawSpeed;
+    return (p_box->Timer2 >= 24);
 }
 
-TbBool draw_world_landmass_shape(struct ScreenBox *box)
+TbBool draw_world_landmass_shape(struct ScreenBox *p_box)
 {
     TbBool to_next;
     short i;
@@ -159,7 +233,7 @@ TbBool draw_world_landmass_shape(struct ScreenBox *box)
         landmap_8C4 = arr[k - 2];
         landmap_8C8 = arr[k - 1];
 
-        for (tmc = 0; tmc < box->Timer2; tmc += 2)
+        for (tmc = 0; tmc < p_box->Timer2; tmc += 2)
         {
             short px, py;
 
@@ -170,8 +244,8 @@ TbBool draw_world_landmass_shape(struct ScreenBox *box)
             py = arr[tmc + 1];
             if ((landmap_8BC != 0) && (px != 0))
             {
-                draw_line_purple_list(box->X + landmap_8BC, box->Y + landmap_8C0,
-                  box->X + px, box->Y + py, 174);
+                draw_line_purple_list(p_box->X + landmap_8BC, p_box->Y + landmap_8C0,
+                  p_box->X + px, p_box->Y + py, 174);
             }
             landmap_8BC = px;
             landmap_8C0 = py;
@@ -180,8 +254,8 @@ TbBool draw_world_landmass_shape(struct ScreenBox *box)
             py = arr[k - tmc - 1];
             if ((landmap_8C4 != 0) && (px != 0))
             {
-                draw_line_purple_list(box->X + landmap_8C4, box->Y + landmap_8C8,
-                  box->X + px, box->Y + py, 174);
+                draw_line_purple_list(p_box->X + landmap_8C4, p_box->Y + landmap_8C8,
+                  p_box->X + px, p_box->Y + py, 174);
             }
             landmap_8C4 = px;
             landmap_8C8 = py;
@@ -192,7 +266,7 @@ TbBool draw_world_landmass_shape(struct ScreenBox *box)
     return to_next;
 }
 
-TbBool draw_world_landmass_projector(struct ScreenBox *box)
+TbBool draw_world_landmass_projector(struct ScreenBox *p_box)
 {
     TbBool to_next;
     short i;
@@ -209,14 +283,14 @@ TbBool draw_world_landmass_projector(struct ScreenBox *box)
         k = word_155110[i];
         arr = dword_1C529C[i];
         tmend = word_155110[i] >> 1;
-        for (tmc = box->Timer2;; tmc += 2)
+        for (tmc = p_box->Timer2;; tmc += 2)
         {
             short px, py;
 
             // Left bottom edges of continents, projector ray
             px = arr[tmc + 0];
             py = arr[tmc + 1];
-            tmiter = 2 * tm_mul + box->Timer2;
+            tmiter = 2 * tm_mul + p_box->Timer2;
             if (tmc >= tmiter || tmc >= tmend)
                 break;
             if ((word_1C488A[i] != 0) && (px != 0))
@@ -224,15 +298,15 @@ TbBool draw_world_landmass_projector(struct ScreenBox *box)
                 tmn = tmc + 2;
                 if (tmn >= tmiter || tmn >= tmend)
                 {
-                    dword_1C48F8 = box->X + px;
-                    dword_1C48FC = box->Y + py;
-                    dword_1C4920 = box->X + word_1C488A[i];
-                    dword_1C4924 = box->Y + word_1C4896[i];
+                    dword_1C48F8 = p_box->X + px;
+                    dword_1C48FC = p_box->Y + py;
+                    dword_1C4920 = p_box->X + word_1C488A[i];
+                    dword_1C4924 = p_box->Y + word_1C4896[i];
                     draw_trig_purple_list(dword_1C48F8, dword_1C48FC,
                         dword_1C4920, dword_1C4924);
                 }
-                draw_line_purple_list(box->X + word_1C488A[i], box->Y + word_1C4896[i],
-                  box->X + px, box->Y + py, 174);
+                draw_line_purple_list(p_box->X + word_1C488A[i], p_box->Y + word_1C4896[i],
+                  p_box->X + px, p_box->Y + py, 174);
             }
             word_1C488A[i] = px;
             word_1C4896[i] = py;
@@ -244,15 +318,15 @@ TbBool draw_world_landmass_projector(struct ScreenBox *box)
                 tmn = tmc + 2;
                 if (tmn >= tmiter || tmn >= tmend)
                 {
-                    dword_1C48F8 = box->X + px;
-                    dword_1C48FC = box->Y + py;
-                    dword_1C4920 = box->X + word_1C48A2[i];
-                    dword_1C4924 = box->Y + word_1C48AE[i];
+                    dword_1C48F8 = p_box->X + px;
+                    dword_1C48FC = p_box->Y + py;
+                    dword_1C4920 = p_box->X + word_1C48A2[i];
+                    dword_1C4924 = p_box->Y + word_1C48AE[i];
                     draw_trig_purple_list(dword_1C48F8, dword_1C48FC,
                         dword_1C4920, dword_1C4924);
                 }
-                draw_line_purple_list(box->X + word_1C48A2[i], box->Y + word_1C48AE[i],
-                  box->X + px, box->Y + py, 174);
+                draw_line_purple_list(p_box->X + word_1C48A2[i], p_box->Y + word_1C48AE[i],
+                  p_box->X + px, p_box->Y + py, 174);
             }
             word_1C48A2[i] = px;
             word_1C48AE[i] = py;
@@ -267,21 +341,21 @@ TbBool draw_world_landmass_projector(struct ScreenBox *box)
             py = arr[tmc - 1];
             if (px != 0)
             {
-                draw_sprite_purple_list(box->X + px - 1, box->Y + py - 1,
+                draw_sprite_purple_list(p_box->X + px - 1, p_box->Y + py - 1,
                   &fe_mouseptr_sprites[12]);
             }
             px = arr[k - tmc + 0];
             py = arr[k - tmc + 1];
             if (px)
-                draw_sprite_purple_list(box->X + px - 1, box->Y + py - 1,
+                draw_sprite_purple_list(p_box->X + px - 1, p_box->Y + py - 1,
                   &fe_mouseptr_sprites[12]);
         }
     }
-    box->Timer2 += 2 * tm_mul;
+    p_box->Timer2 += 2 * tm_mul;
     return to_next;
 }
 
-TbBool draw_world_country_borders(struct ScreenBox *box)
+TbBool draw_world_country_borders(struct ScreenBox *p_box)
 {
     short i;
 
@@ -297,8 +371,8 @@ TbBool draw_world_country_borders(struct ScreenBox *box)
             px = landmap_2B4[i + 0];
             py = landmap_2B4[i + 1];
             if (px != 0)
-                draw_line_purple_list(box->X + landmap_8BC, box->Y + landmap_8C0,
-                    box->X + px, box->Y + py, 247);
+                draw_line_purple_list(p_box->X + landmap_8BC, p_box->Y + landmap_8C0,
+                    p_box->X + px, p_box->Y + py, 247);
             else
                 i += 2;
             landmap_8BC = landmap_2B4[i + 0];
@@ -306,28 +380,28 @@ TbBool draw_world_country_borders(struct ScreenBox *box)
         }
 
         lbDisplay.DrawFlags = Lb_SPRITE_TRANSPAR4;
-        draw_line_purple_list(box->X + 1, box->Y + 181,
-          box->X + 70, box->Y + 181, 247);
-        draw_line_purple_list(box->X + 109, box->Y + 181,
-          box->X + 247, box->Y + 181, 247);
-        draw_line_purple_list(box->X + 352, box->Y + 181,
-          box->X + 404, box->Y + 181, 247);
-        draw_line_purple_list(box->X + 410, box->Y + 181,
-          box->X + 436, box->Y + 181, 247);
-        draw_line_purple_list(box->X + 446, box->Y + 181,
-          box->X + 482, box->Y + 181, 247);
-        draw_line_purple_list(box->X + 490, box->Y + 181,
-            box->X + box->Width - 2, box->Y + 181, 247);
+        draw_line_purple_list(p_box->X + 1, p_box->Y + 181,
+          p_box->X + 70, p_box->Y + 181, 247);
+        draw_line_purple_list(p_box->X + 109, p_box->Y + 181,
+          p_box->X + 247, p_box->Y + 181, 247);
+        draw_line_purple_list(p_box->X + 352, p_box->Y + 181,
+          p_box->X + 404, p_box->Y + 181, 247);
+        draw_line_purple_list(p_box->X + 410, p_box->Y + 181,
+          p_box->X + 436, p_box->Y + 181, 247);
+        draw_line_purple_list(p_box->X + 446, p_box->Y + 181,
+          p_box->X + 482, p_box->Y + 181, 247);
+        draw_line_purple_list(p_box->X + 490, p_box->Y + 181,
+            p_box->X + p_box->Width - 2, p_box->Y + 181, 247);
 
         lbDisplay.DrawFlags = 0;
         if (byte_1C4888 == 3)
-            copy_box_purple_list(box->X - 3, box->Y - 3,
-               box->Width + 6, box->Height + 6);
+            copy_box_purple_list(p_box->X - 3, p_box->Y - 3,
+               p_box->Width + 6, p_box->Height + 6);
 
     return true;
 }
 
-void draw_world_detached_city_square(struct ScreenBox *box, short city)
+void draw_world_detached_city_square(struct ScreenBox *p_box, short city)
 {
     lbDisplay.DrawFlags = 0;
     draw_line_purple_list(cities[city].X - 8, cities[city].Y - 8,
@@ -340,7 +414,7 @@ void draw_world_detached_city_square(struct ScreenBox *box, short city)
       cities[city].X - 8, cities[city].Y - 8, 174);
 }
 
-void draw_world_cities_names(struct ScreenBox *box)
+void draw_world_cities_names(struct ScreenBox *p_box)
 {
     int city;
 
@@ -385,8 +459,8 @@ void draw_world_cities_names(struct ScreenBox *box)
             }
         }
 
-        px = ms_x - cities[city].X - box->X - 1;
-        py = ms_y - cities[city].Y - box->Y - 1;
+        px = ms_x - cities[city].X - p_box->X - 1;
+        py = ms_y - cities[city].Y - p_box->Y - 1;
         if ((cities[city].Flags & (CitF_Unkn10|CitF_Unkn01)) || (byte_15511C == 2)
            || (city == unkn_city_no) || ((px >= 0) && (py >= 0) && (px <= 3) && (py <= 3)))
         {
@@ -412,7 +486,7 @@ void draw_world_cities_names(struct ScreenBox *box)
             landmap_8BC = my_string_width(locstr);
             text = loctext_to_gtext(locstr);
 
-            if (cities[city].X + 5 + landmap_8BC < box->Width - 2) {
+            if (cities[city].X + 5 + landmap_8BC < p_box->Width - 2) {
                 px = cities[city].X + 5;
                 py = cities[city].Y - 3;
             } else {
@@ -429,16 +503,16 @@ void draw_world_cities_names(struct ScreenBox *box)
     }
 }
 
-void input_world_cities(struct ScreenBox *box)
+void input_world_cities(struct ScreenBox *p_box)
 {
-    if (byte_15511C < 3 && lbDisplay.LeftButton && mouse_move_over_box(box))
+    if (byte_15511C < 3 && lbDisplay.LeftButton && mouse_move_over_box(p_box))
     {
         short ms_x, ms_y;
 
         lbDisplay.LeftButton = 0;
         ms_x = lbDisplay.GraphicsScreenHeight < 400 ? 2 * lbDisplay.MMouseX : lbDisplay.MMouseX;
         ms_y = lbDisplay.GraphicsScreenHeight < 400 ? 2 * lbDisplay.MMouseY : lbDisplay.MMouseY;
-        landmap_8C4 = find_closest_city(ms_x - box->X, ms_y - box->Y);
+        landmap_8C4 = find_closest_city(ms_x - p_box->X, ms_y - p_box->Y);
         if (unkn_city_no != landmap_8C4)
         {
             unkn_city_no = landmap_8C4;
@@ -452,12 +526,12 @@ void input_world_cities(struct ScreenBox *box)
     }
 }
 
-ubyte show_world_landmap_box(struct ScreenBox *box)
+ubyte show_world_landmap_box(struct ScreenBox *p_box)
 {
     short i;
     TbBool next_state;
 
-    if (box->Timer2 == 0xFFFF)
+    if (p_box->Timer2 == 0xFFFF)
     {
         for (i = 0; i < 6; i++)
         {
@@ -472,7 +546,7 @@ ubyte show_world_landmap_box(struct ScreenBox *box)
             word_1C48AE[i] = arr[k - 1];
         }
         byte_1C4888 = 1;
-        box->Flags &= ~GBxFlg_Unkn0080;
+        p_box->Flags &= ~GBxFlg_Unkn0080;
     }
 
     if (is_key_pressed(KC_C, KMod_DONTCARE))
@@ -507,7 +581,7 @@ ubyte show_world_landmap_box(struct ScreenBox *box)
         }
     }
 
-    if (box->Flags & GBxFlg_Unkn0080)
+    if (p_box->Flags & GBxFlg_Unkn0080)
     {
         for (i = 0; i < 6; i++)
         {
@@ -522,42 +596,42 @@ ubyte show_world_landmap_box(struct ScreenBox *box)
             word_1C48AE[i] = arr[k - 1];
         }
         byte_1C4888 = 0;
-        box->Flags &= ~GBxFlg_Unkn0080;
+        p_box->Flags &= ~GBxFlg_Unkn0080;
     }
 
     switch (byte_1C4888)
     {
     case 0:
-        next_state = draw_world_landmass_projector_startup(box);
+        next_state = draw_world_landmass_projector_startup(p_box);
         if (next_state)
         {
-            box->Timer2 = 0;
+            p_box->Timer2 = 0;
             byte_1C4888++;
         }
         return 0;
     case 1:
         if (!IsSamplePlaying(0, 113, 0))
             play_sample_using_heap(0, 110, 127, 64, 100, 0, 1);
-        next_state = draw_world_landmass_shape(box);
-        next_state &= draw_world_landmass_projector(box);
+        next_state = draw_world_landmass_shape(p_box);
+        next_state &= draw_world_landmass_projector(p_box);
         if (next_state)
         {
             byte_1C4888++;
-            box->Timer2 = 0;
-            copy_box_purple_list(box->X - 3, box->Y - 3, box->Width + 6, box->Height + 6);
+            p_box->Timer2 = 0;
+            copy_box_purple_list(p_box->X - 3, p_box->Y - 3, p_box->Width + 6, p_box->Height + 6);
         }
         return 1;
     case 2:
     case 3:
-        next_state = draw_world_country_borders(box);
+        next_state = draw_world_country_borders(p_box);
         if (next_state)
             byte_1C4888++;
         return byte_1C4888;
     case 4:
-        LbScreenSetGraphicsWindow(box->X + 1, box->Y + 1,
-          box->Width - 2, box->Height - 2);
-        my_set_text_window(box->X + 1, box->Y + 1,
-          box->Width - 2, box->Height - 2);
+        LbScreenSetGraphicsWindow(p_box->X + 1, p_box->Y + 1,
+          p_box->Width - 2, p_box->Height - 2);
+        my_set_text_window(p_box->X + 1, p_box->Y + 1,
+          p_box->Width - 2, p_box->Height - 2);
 
         if (word_1C6E08 != cities[unkn_city_no].X
          || word_1C6E0A != cities[unkn_city_no].Y)
@@ -604,19 +678,19 @@ ubyte show_world_landmap_box(struct ScreenBox *box)
         }
 
         lbDisplay.DrawFlags = Lb_SPRITE_TRANSPAR4;
-        draw_line_purple_list(0, word_1C6E0A, box->Width - 3, word_1C6E0A, 87);
-        draw_line_purple_list(word_1C6E08, 0, word_1C6E08, box->Height - 3, 87);
+        draw_line_purple_list(0, word_1C6E0A, p_box->Width - 3, word_1C6E0A, 87);
+        draw_line_purple_list(word_1C6E08, 0, word_1C6E08, p_box->Height - 3, 87);
 
         // Square around orbital station
-        draw_world_detached_city_square(box, 29);
+        draw_world_detached_city_square(p_box, 29);
         break;
     default:
         return byte_1C4888;
     }
 
-    draw_world_cities_names(box);
+    draw_world_cities_names(p_box);
     LbScreenSetGraphicsWindow(0, 0, lbDisplay.GraphicsScreenWidth, lbDisplay.GraphicsScreenHeight);
-    input_world_cities(box);
+    input_world_cities(p_box);
 
     return 4;
 }
